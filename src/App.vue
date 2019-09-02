fe2477bcf674f6a870554fdf2aa8febeffda6a24
Maksim Severukhin <maksim_severukhin@epam.com>
2019-09-02 11:07:49 +0300

    getters

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,49 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      A: {{ getA }}
+    </div>
+    <div>
+      B: {{ getAB }}
+    </div>
+    <div>
+      C: {{ c }}
+    </div>
+    <div>
+      D: {{ getA.d }}
+    </div>
+    <div>
+      AB2: {{ ab2 }}
+    </div>
+    <button @click="mutateC(x++)">Mutate</button>
+    <button @click="mutateD(y++)">MutateD</button>
+    <button @click="mutateD2(z++)">MutateD2</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { mapGetters, mapMutations, mapState } from 'vuex'
+
+let temp;
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      x: 1,
+      y: 1,
+      z: 1
+    }
+  },
+  computed: {
+    ...mapState({c : 'c', d: 'a/d' }),
+    ...mapGetters(['getA', 'getAB', 'getAB2']),
+    ab2 () {
+      return this.getAB2(this.y)
+    }
+  },
+  methods: {
+    ...mapMutations(['mutateC', 'mutateD', 'mutateD2'])
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,66 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-
-import { PLP } from './PLP'
-import { SRP } from './SRP'
-
 Vue.use(Vuex)
 
-export const store = new Vuex.Store({
-  modules: {
-    PLP,
-    SRP
+let pref = null
+
+const getters = {
+  getA (state) {
+    console.log('getA');
+    
+    return state.a
+  },
+  getAB (_, { getA }) {
+    console.log('getAB');
+    
+    return getA.b
+  },
+  getAB2 (_, { getAB }) {
+    console.log('getAB2');
+
+    const result = val => {
+      console.log('getAB2Inner');
+
+      return getAB + val
+    }
+
+    pref = {
+      ab: getAB,
+      result
+    }
+
+    return result
+  },
+  getBlabla (state) {
+    console.log('GetBlaBla');
+    
+    return 0
   }
+}
+
+const state = {
+  a: {
+    b: '42',
+    d: 1
+  },
+  c: 'bla'
+}
+
+const mutations = {
+  mutateC (state, val) {
+    state.c = val
+  },
+  mutateD (state, val) {
+    state.a.d = val
+  },
+  mutateD2 (state, val) {
+    Vue.set(state.a, 'd', val)
+  }
+}
+
+export const store = new Vuex.Store({
+  state,
+  getters,
+  mutations
 })
